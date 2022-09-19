3919e2361ae0c452f1f6eba7efdc3c8cd23b8702
danie <71136486+lajbel@users.noreply.github.com>
2022-09-19 12:40:01 +0000

    basically finished home page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ export default class Nav extends Component {
 
     render() {
         return (
-            <nav class="h-fit p-5 grid grid-cols-1 grid-rows-2 items-center font-unscii bg-[#fcef8d] md:grid-cols-2 md:grid-rows-1">
+            <nav class="h-fit p-5 grid grid-cols-1 grid-rows-2 items-center font-unscii bg-[#fcef8d] md:grid-cols-2 md:grid-rows-1 mb-4">
                 <img class="inline-block h-10 mx-auto md:mx-0" src="images/Logo.png"></img>
 
                 <ul class="flex flex-row justify-center inline-block md:justify-around">
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,18 +2,30 @@ import Nav from "../components/Nav.tsx";
 
 export default function Home() {
     return (
-        <div class="w-screen h-screen bg-repeat bg-[#fbc0f3] bg-mark-pattern text-[#1f102a]">
-            <div class="p-4 mx-auto max-w-screen-lg h-screen bg-[#fcef8d]">
+        <div class="w-screen h-screen bg-repeat bg-[#fbc0f3] bg-mark-pattern text-[#1f102a] font-unscii">
+            <div id="wrapper" class="px-4 max-w-screen-lg h-screen overflow-auto bg-[#fcef8d] md:mx-auto">
                 <Nav />
 
                 <img class="mx-auto" src="/images/Logo.png" alt="MarkJam"></img>
 
-                <p class="text-center font-unscii p-8">
+                <p class="text-center p-8">
                     MarkJam it's a jam about make games with Mark. Yes, the
                     famous Kaboom character
                 </p>
 
-                <img class="mx-auto" src="images/markdefault.png"></img>
+                <img class="mx-auto mb-6" src="images/markdefault.png"/>
+
+                <div class="inline-flex w-full justify-center flex-col text-[#fff] md:flex-row">
+                    <a
+                        class="bg-[#5865F2] h-fit px-4 py-3 mx-4 rounded my-2"
+                        href="https:kaboomjs.com/discord">Join Discord
+                    </a>
+
+                    <a
+                        class="bg-[#fa5c5c] h-fit px-4 py-3 mx-4 rounded my-2"
+                        href="https:kaboomjs.com/discord">Jam on Itch
+                    </a>
+                </div>
             </div>
         </div>
     );
