8fd60648992572dff19987b39f7be5306960be42
danie <71136486+lajbel@users.noreply.github.com>
2022-09-27 14:24:05 +0000

    misc

--- components/Jam.tsx
+++ components/Jam.tsx
@@ -6,9 +6,11 @@ interface JamProps {
 
 export default function Jam(props: JamProps) {
     return (
-        <a href={props.url} target="_blank">
-            <h1>{props.title}</h1>
-            <p>{props.theme}</p>
-        </a>
+        <div class="p-4 bg-[#d46eb3] rounded">
+            <a href={props.url} target="_blank">
+                <h1>{props.title}</h1>
+                <p>{props.theme}</p>
+            </a>
+        </div>
     );
 }
